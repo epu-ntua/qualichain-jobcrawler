591c107a96abbda29a7c166838b10b36bdd774e9
kapsali29 <kapsali29@gmail.com>
2020-02-08 19:48:16 +0200

    some changes on js_middleware for skywalker website crawling

--- jobcrawler/middlewares/js_middleware.py
+++ jobcrawler/middlewares/js_middleware.py
@@ -6,13 +6,18 @@ from selenium.webdriver.chrome.options import Options
 
 class JSMiddleware(object):
     def process_request(self, request, spider):
-        if spider.allowed_domains == ['skywalker.gr'] and 'aggelia/ergasias' not in request.url:
-            options = Options()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+        """This middleware is applied on skywalker's start urls that are protected with JS"""
 
+        # check response url
+        if spider.allowed_domains == ['skywalker.gr'] and 'aggelia/ergasias' not in request.url:
+
+            options = Options()  # Set webdriver options
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+
+            # Initialize webdriver for Chrome
             driver = webdriver.Chrome('C:/webdrivers/chromedriver.exe', chrome_options=options)
             driver.get(request.url)
 
-            body = driver.page_source
+            body = driver.page_source  # get page source
             return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
